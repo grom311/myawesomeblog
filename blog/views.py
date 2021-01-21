88f3f7caf0685aab6e9aab533c26654915182ba4
grom311 <grom311@yandex.ru>
2021-01-21 23:53:03 +0300

    pgdb

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+
+
+def showblog(request):
+    posts = Event.objects
+    return render(request, 'blog/blog.html', {'posts': posts})
